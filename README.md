768cf3a4a6aadafbcf6454302fec9ee84bcd86d2
hschoenburg <hschoenburg@gmail.com>
2016-11-22 13:28:40 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+To run the app:
+npm start from the /dribbble_client directory.
+
+The start command accepts command line arguments with the following format:
+npm start + "script type" + "item id", where "script type" is one of 
+["usershots",
+ "shot",
+ "attachments",
+ "everyone",
+ "popular",
+ "debuts"]
+
+and "item id" is a userId or a shotId.
+
+Examples:
+npm start usershots 43762, where 43762 is the userId
+npm start shot , where is the shotId
+npm start attachments , where  is the shotId
+npm start everyone
+npm start popular
+npm start debuts
+
+When any varation of the start commands is run, the client will download all images related to the command line parameters, and then store the images on the file system in the following directory: /dribbble_client/dribbble_saved_photos. This directory, its sub-directories, and the image files have the following naming scheme:
+username/usershots-userid/43762-shotid/771815
+username/usershots-[userId]/ , where 
+/everyone-([date])
+/debuts-([date])
+/popular-([date])
+
+When a single iteration of the script has completed, the following line will be printed to the console, and then the process will exit: "Fetch completed. Exiting process."
+
+
+
+**Useful Dribbble client resources:**
+- http://developer.dribbble.com/v1
+- http://developer.dribbble.com/v1/#authentication
+- https://www.npmjs.com/package/dribbble-api
+- https://gist.github.com/jim/668086
+- http://lab.tylergaw.com/jribbble/
+- https://github.com/tylergaw/jribbble
---
+++ cloud/app.js
@@ -0,0 +1,236 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var dribbbleApi = require('dribbble-api');
+var express   = require('express');
+var request = require('request');
+// var Promise = require('promise');
+
+var base_dribbble_url = 'https://api.dribbble.com/v1/';
+var dribbble_client_access_token = '8da27c7ff5a81648b7391fcf8387e34df74ef36bf65252531a4d1ada331b10c7';
+var saved_images_directory = "dribbble_saved_photos";
+var todaysDate = new Date();
+
+/*
+Things to fix:
+Fetch everyone, popular, and debuts sequentially, not in parallel and update saved_images_directory
+*/
+
+// Configure Express:
+var app       = module.exports = express();
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+ 
+var dribbble = new dribbbleApi();
+
+// Requests:
+var requestCallback = function(err, res, json, paging, completion) {
+	var shotResultsArray = [];
+
+  if (Array.isArray(json)) {
+    shotResultsArray = shotResultsArray.concat(json);
+
+    // Track results length, so we know when to exit the process in fetchCompletion():
+    streamRequestCount+=shotResultsArray.length;
+
+    // Iterate over shotResultsArray, download each image, and save it to a directory:
+    for (var i = 0; i < shotResultsArray.length; i++) {
+    	// In the case where we're fetching shots for a specific user, dribbble does not return a "user" attribute:
+    	var username = shotResultsArray[i].user ? shotResultsArray[i].user.username : process.argv[2];
+    	var user_id = shotResultsArray[i].user ? shotResultsArray[i].user.id : process.argv[3];
+
+    	var fileName = "username:" + username + "-userid:" + user_id + "-shotid:" + shotResultsArray[i].id;
+    	fileName = fileName.replace(/ /g,"_").replace("\\","_").replace("/","_").replace("\"","_");
+
+    	// HD image is available:
+    	if (shotResultsArray[i].images && shotResultsArray[i].images.hidpi) {
+    		saveFileToDirectory(shotResultsArray[i].images.hidpi, fileName, saved_images_directory);
+    	// Only "normal" size image is available:
+    	} else if (shotResultsArray[i].images) {
+    		saveFileToDirectory(shotResultsArray[i].images.normal, fileName, saved_images_directory);
+    	// Image is an attachment:
+    	} else {
+    		saveFileToDirectory(shotResultsArray[i].url, fileName, saved_images_directory);
+    	}
+    }
+  } else {
+  	// We've fetched a specific shot:
+  	if (json.images) {
+  		streamRequestCount++;
+  		var fileName = "username:" + json.user.username + "-userid:" + json.user.id + "-shotid:" + json.id;
+    	fileName = fileName.replace(/ /g,"_").replace("\\","_").replace("/","_").replace("\"","_");
+
+    	saved_images_directory = saved_images_directory + "/username:" + json.user.username + "-userid:" + json.user.id;
+
+  		if (json.images.hidpi) {
+  			saveFileToDirectory(json.images.hidpi, fileName, saved_images_directory);
+  		} else {
+  			saveFileToDirectory(json.images.normal, fileName, saved_images_directory);
+  		}
+  	// We've fetched a user profile:
+  	} else {
+  		//completion(json.username);
+  		return;
+  	}
+  }
+
+  if (paging.next) {
+    paging.next(requestCallback);
+  }
+}
+
+var baseRequest = function(requestBase, page, callback) {
+	var url = base_dribbble_url + requestBase;
+
+	// Pagination:
+	if (typeof(page) === 'number') {
+		var per_page = 100;
+
+		url = url + "&page=" + page + "&per_page=" + per_page;
+	}
+
+	request(url, function(err, res, body) {
+		var paging = {};
+    var pages;
+
+    if (!body) {
+      return callback(new Error('No response from Dribbble API'));
+    }
+
+    try {
+      body = JSON.parse(body);
+    } catch(e) {}
+
+    if (body.length >= per_page) {
+      paging.next = function(callback) {
+        page = page + 1;
+        baseRequest(requestBase, page, callback);
+      };
+    }
+
+	  if (page > 1) {
+	    paging.previous = function(callback) {
+	      page = page - 1;
+	      baseRequest(requestBase, page, callback);
+	    };
+	  }
+
+    callback(err, res, body, paging);
+	});
+}
+ 
+var fetchCompletion = function() {
+	console.log("Number of images still downloading: " + streamRequestCount);
+	if (streamRequestCount <= 0) {
+		console.log("Fetch completed. Exiting process.");
+		process.exit();
+	}
+}
+
+// Debuts:
+var fetchDebuts = function() {
+	saved_images_directory = saved_images_directory + "/debuts-(" + todaysDate + ")"
+	baseRequest("shots?list=debuts&access_token=" + dribbble_client_access_token, 0, requestCallback);
+}
+
+// Popular:
+var fetchPopular = function() {
+	saved_images_directory = saved_images_directory + "/popular-(" + todaysDate + ")"
+	baseRequest("shots?list=popular&access_token=" + dribbble_client_access_token, 0, requestCallback);
+}
+
+// Everyone:
+var fetchEveryone = function() {
+	saved_images_directory = saved_images_directory + "/everyone-(" + todaysDate + ")"
+	baseRequest("shots?list=everyone&access_token=" + dribbble_client_access_token, 0, requestCallback);
+}
+
+var fetchAll = function() {
+	// fetchDebuts();
+	// fetchPopular();
+	fetchEveryone();
+}
+
+// Fetch a specific User's profile:
+var fetchUser = function(userId) {
+	baseRequest("users/" + userId + "?access_token=" + dribbble_client_access_token, null, requestCallback);
+}
+
+// Fetch shots by specific Users:
+var fetchShotsByUser = function(userId, directory) {
+	baseRequest("users/" + userId + "?access_token=" + dribbble_client_access_token, null, function(err, res, json) {
+		saved_images_directory = saved_images_directory + "/username:" + json.username + "-userid:" + json.id;
+		baseRequest("users/" + userId + "/shots" + "?access_token=" + dribbble_client_access_token, 0, requestCallback);
+	});
+}
+
+// Fetching a specific shot
+var fetchShot = function(shotId) {
+	baseRequest("shots/" + shotId + "?access_token=" + dribbble_client_access_token, null, requestCallback);
+}
+
+// Fetching a shot's attachments
+var fetchAttachment = function(shotId) {
+	baseRequest("shots/" + shotId + "?access_token=" + dribbble_client_access_token, null, function(err, res, json) {
+		saved_images_directory = saved_images_directory + "/username:" + json.user.username + "-userid:" + json.user.id;
+		baseRequest("shots/" + shotId + "/attachments" + "?access_token=" + dribbble_client_access_token, null, requestCallback);
+	});
+}
+
+// Methods can be run from the command line:
+var scriptType = process.argv[2];
+if (scriptType) {
+	var unitType = process.argv[2];
+	var unitId = process.argv[3];
+
+	if (unitType && unitId) {
+		if (unitType == "usershots") {
+			console.log("Fetching shots for a specific user with id:" + unitId);
+			fetchShotsByUser(unitId);
+		} else if (unitType == "shot") {
+			console.log("Fetching a specific shot with id:" + unitId);
+			fetchShot(unitId);
+		} else if (unitType == "attachments") {
+			console.log("Fetching attachments for a specific shot with id:" + unitId);
+			fetchAttachment(unitId);
+		} else {
+			console.log("Error: missing some command line arguments. Try again.");
+		}
+	} else if (unitType == "everyone") {
+		console.log("Fetching today's results for 'Everyone'.");
+		fetchEveryone();
+	} else if (unitType == "popular") {
+		console.log("Fetching today's results for 'Popular'.");
+		fetchPopular();
+	} else if (unitType == "debuts") {
+		console.log("Fetching today's results for 'Debuts'.");
+		fetchDebuts();
+	}
+} else {
+	console.log("Error: missing some command line arguments. Try again.");
+	process.exit();
+	//fetchAll();
+}
+
+
+var streamRequestCount = 0;
+
+var saveFileToDirectory = function(fileUrl, fileName, directory) {
+	if (!fs.existsSync(directory)){
+	  fs.mkdirSync(directory);
+	}
+
+	var file = fs.createWriteStream(directory + "/" + fileName /*+  ".jpg"*/);
+	var request = https.get(fileUrl, function(response) {
+	  response.pipe(file);
+	  response.on('end', function () { 
+			streamRequestCount--;
+			fetchCompletion();
+		});
+	});
+
+	request.on('error', function (e) {
+  	console.log(e);
+  });
+}
+
